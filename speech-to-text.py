dda38d68ffe863f51f50a6147b712de317673999
margoxt <angelicatinio14@gmail.com>
2025-08-18 19:45:20 +0800

    initial commit

---
+++ speech-to-text.py
@@ -0,0 +1,14 @@
+#Step 1: Allow the computer's microphone to listen for audio input
+def record_text():
+    return
+
+#Step 2: Output the speech to text in a file
+def output_text((text)):
+    return
+
+#Step 3: Put the steps in a while loop to convert speech to text
+while(1):
+    text = record_text()
+    output_text(text)
+
+    print("Wrote Text")
