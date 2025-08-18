bae9ca98f8ed1003a3d1cdd36759cc0996a4d6e5
margoxt <angelicatinio14@gmail.com>
2025-08-18 20:56:00 +0800

    initialize the recognizer and output string

--- speech-to-text.py
+++ speech-to-text.py
@@ -1,13 +1,47 @@
-#Step 1: Allow the computer's microphone to listen for audio input
+import speech_recognition as sr             #This makes the program listen to audio from mic or file & converts speech into text
+import pyttsx3                              #This makes the program speak text out loud.
+
+
+#Initialize the recognizer. This is the object that is used to interact with the microphone
+r = sr.Recognizer()
+
+#Step 1: Allow the computer's microphone to listen for audio input and turn it into a string
 def record_text():
+    # Loop in case of errors (unidentifiable audio)
+    while True: 
+        try:
+            #Use the microphone as a source of input
+            with sr.Microphone() as audio_source:    
+
+                #Prepare recognizer to receive input
+                r.adjust_for_ambient_noise(audio_source, duration=0.2)
+
+                #Use the recognizer's listen function
+                captured_audio = r.listen(audio_source)
+
+                #The audio2 will be then used by the recognize_google function to recognize the spoken words and make it string
+                r_text = r.recognize_google(captured_audio)
+
+                return r_text
+
+        except sr.RequestError as error:
+            print("Could not request results; {0}".format(error))
+
+        except sr.UnknownValueError:
+            print("Unknown Error Occured")
+
     return
 
-#Step 2: Output the speech to text in a file
-def output_text((text)):
+#Step 2: Take the string from step 1 and output it as a text file
+def output_text(text):
+
+    # Open a text file using the open function, this will allow us to access the output.txt file in the directory of the program. If there's no file, the program will create one automatically
+    f = open("output.txt", "a")
+    
     return
 
-#Step 3: Put the steps in a while loop to convert speech to text
-while(1):
+#Step 3: The while loop calls the two functions on repeat
+while(True):
     text = record_text()
     output_text(text)
 
